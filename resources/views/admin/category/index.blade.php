78bf05309a6da95852f53bde1c55f22966cebdd0
Kenneth Ekow Inkum <xanthosoma1989@gmail.com>
2022-11-27 03:31:39 +0000

    Created categories

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-header">
+                <h4>Category
+                    <a href="">Add Category</a>
+                </h4>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
+    Route::get('dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index']);
+
+    //Category Routes
+    Route::get('category',[App\Http\Controllers\Admin\CategoryController::class, 'index']);
 });
